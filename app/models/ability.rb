b963d1f73bced804c6a771e3a40af35b7a7e7418
zOmegad <simonrichard1998@gmail.com>
2018-11-19 16:13:13 +0100

    rails, User crud and sessions

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    alias_action :create, :read, :update, :destroy, to: :crud # Pour ne pas se répéter
+
+    can [:read, :create], User # Seul les actions :index, :show sont accessible publiquement
+
+    if user.id
+      can [:crud], User, {user_id: user.id} # Il faut être le propriétaire d'un enregistrement pour l'éditeur
+    end
+  end
+end
